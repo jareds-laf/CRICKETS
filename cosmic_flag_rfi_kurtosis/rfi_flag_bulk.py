68d3a0d9ee4ebc5cdeffc840d7cff48d109c3b0e
jareds-laf <106609477+jareds-laf@users.noreply.github.com>
2023-07-14 10:38:03 -0600

    Create rfi_flag_bulk.py
    This file will be the file you run the analysis from! I'm unfamiliar with how to get argparse to work, so I will be doing a lot of experimentation with it.

---
+++ cosmic_flag_rfi_kurtosis/rfi_flag_bulk.py
@@ -0,0 +1,22 @@
+import argparse
+from analysis_functions import get_mask_kurtosis
+
+parser = argparse.ArgumentParser(
+                    description='Flag RFI heavy frequency channels based on the kurtosis of each channel.')
+
+parser.add_argument('filename', help='Path to input filterbank file', required=True)
+# parser.add_argument('filename', help='Path to input filterbank file')
+
+args = parser.parse_args()
+
+
+
+print("Argument values:")
+print(args.filename, type(args.filename)
+
+
+
+# parser.add_argument('filename')           # positional argument
+# parser.add_argument('-c', '--count')      # option that takes a value
+# parser.add_argument('-v', '--verbose',
+#                     action='store_true')  # on/off flag
